4023838cc5a430585a7a9a8bcf50b82994363628
naakhurz <157807981+naakhurz@users.noreply.github.com>
2024-06-15 17:25:39 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,21 +1,19 @@
 // See https://github.com/typicode/json-server#module
-import { create, router as _router, defaults, rewriter } from "json-server";
-const server = create();
-const router = _router("db.json");
-const middlewares = defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(
-  rewriter({
-    "/api/*": "/$1",
-    "/product/:resource/:id/show": "/:resource/:id",
-  })
-);
-server.use(router);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/product/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log("JSON Server is running");
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-export default server;
+module.exports = server
