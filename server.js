5230e0ca5b5a73a6fbd01e1b4c8445e7c18bd92d
naakhurz <jaggerrios2@gmail.com>
2024-06-15 17:07:18 -0300

    Second commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// See https://github.com/typicode/json-server#module
+import { create, router as _router, defaults, rewriter } from "json-server";
+const server = create();
+const router = _router("db.json");
+const middlewares = defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  rewriter({
+    "/api/*": "/$1",
+    "/product/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+export default server;
